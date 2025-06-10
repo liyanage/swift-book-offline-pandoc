da53fa6db64d87d12d029dfeca39f530ad9037fc
Marc Liyanage <reg.gitcommit@entropy.ch>
2025-06-09 21:41:33 -0700

    raise macOS deployment target

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "swift-book-offline",
     platforms: [
-        .macOS(.v14)
+        .macOS(.v15)
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
